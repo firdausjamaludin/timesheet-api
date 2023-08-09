1cf660dd604cef1ca86864dae994ef3237e15bed
Firdaus J <firdausjmldn@gmail.com>
2023-08-10 02:02:02 +0800

    Initial commit

---
+++ src/main/java/com/example/timesheetapi/TimesheetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.timesheetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timesheetapi/TimesheetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timesheetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
